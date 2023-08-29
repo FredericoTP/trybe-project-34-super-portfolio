4c8030a26bad5a6fb236bb01b9626b3af54ec27b
Frederico Tavares <fredericotp@hotmail.com>
2023-08-29 18:21:55 -0300

    Feat: Requisito 5

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Project, CertifyingInstitution, Certificate
+
+
+class CertificateInline(admin.StackedInline):
+    model = Certificate
+
+
+class CertifyingInstitutionAdmin(admin.ModelAdmin):
+    inlines = [CertificateInline]
+
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(CertifyingInstitution, CertifyingInstitutionAdmin)
+admin.site.register(Certificate)
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, Project
+from .models import Profile, Project, CertifyingInstitution, Certificate
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -12,3 +12,34 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = "__all__"
+
+
+class NestedCertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ["id", "name", "profiles"]
+
+
+class CertifyingInstitutionSerializer(serializers.ModelSerializer):
+    certificates = NestedCertificateSerializer(many=True)
+
+    class Meta:
+        model = CertifyingInstitution
+        fields = ["id", "name", "url", "certificates"]
+
+    def create(self, validated_data):
+        certificate_data = validated_data.pop("certificates")
+        institution = CertifyingInstitution.objects.create(**validated_data)
+
+        for certificate_data in certificate_data:
+            Certificate.objects.create(
+                certifying_institution=institution,
+                **certificate_data,
+            )
+        return institution
